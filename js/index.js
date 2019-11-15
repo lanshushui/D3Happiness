a8ef4267aae73e22c55e790d34ea9cb2969354e9
lanshushui <1256087403@qq.com>
2019-11-15 21:58:12 +0800

    完善雷达图

--- js/index.js
+++ js/index.js
@@ -50,30 +50,60 @@
    */
 
    function drawChinaRadar(){
-    var radarconfig={
-      width: 300,
-      height: 300,
-      fill_opacity: 0.1,
-      levels: 5,
-      maxR: 140
-    }
+      var radarconfig={
+        width: 320,
+        height: 320,
+        fill_opacity: 0.1,
+        levels: 5,//多少个同心圆
+        slicenum: 5,//多少个切片
+        labelFactor: 1.25,
+        maxR: 120,
+        strokeWidth: 2, 
+        angleSlice : Math.PI * 2 / 5
+      }
+    var allAxis=['HappinessScore','Health','Freedom','Trust','Generosity'];
+
+    ////创造数据
+    var chinadata=csv2015.filter( function(value, key) {
+          return value.Country == 'China'; //只选中国
+        })[0];
+    var data = [];
+    var arr = [];
+    for (let i in chinadata) {
+      if(!allAxis.includes(i)) continue; 
+      let o = {};
+        o['axis'] = chinadata[i]; //即添加了key值也赋了value值 o[i] 相当于o.name 此时i为变量
+        arr.push(o);
+      } 
+      data.push(arr);
+      
+    //范围尺
+    var rScale = d3.scaleLinear()
+    .range([0, radarconfig.maxR])
+    .domain([0, 1]);
+
+    
     //创造SVG
     var width=radarconfig.width;
     var height=radarconfig.height;
     var svg = d3.select(".svg-radar")
-                .append("svg")
-                .attr("width", width)
-                .attr("height",height)
-                .attr("class", "china-radar");
-    //Draw the background circles
-    var axisGrid = svg.append("g").attr("transform", "translate(" + width/2+ "," + width/2 +")");
+    .append("svg")
+    .attr("width", width)
+    .attr("height",height)
+    .attr("class", "china-radar");
+    //雷达的区域
+    var g = svg.append("g").attr("transform", "translate(" + width/2+ "," + width/2 +")");
 
-    //Filter for the outside glow
-    var filter = axisGrid.append('defs').append('filter').attr('id','glow'),
+    
+    //这段加上下面一段代码才会出现同心圆
+    var filter = g.append('defs').append('filter').attr('id','glow'),
     feGaussianBlur = filter.append('feGaussianBlur').attr('stdDeviation','2.5').attr('result','coloredBlur'),
     feMerge = filter.append('feMerge'),
     feMergeNode_1 = feMerge.append('feMergeNode').attr('in','coloredBlur'),
     feMergeNode_2 = feMerge.append('feMergeNode').attr('in','SourceGraphic');
+
+    //背景坐标svg
+    var axisGrid = g.append("g").attr("class", "axisWrapper");
 
     axisGrid.selectAll(".levels")
     .data(d3.range(1,radarconfig.levels+1).reverse())
@@ -89,20 +119,81 @@
     //上面两端代码出现圆形背景
     /////
     /////
-    //Text indicating at what % each level is
-     axisGrid.selectAll(".axisLabel")
-     .data(d3.range(1,radarconfig.levels+1).reverse())
-     .enter().append("text")
-     .attr("class", "axisLabel")
-     .attr("x", 0)
-     .attr("y", function(d){return -d*radarconfig.maxR/radarconfig.levels;})
-     .attr("dy", "0.4em")
-     .style("font-size", "10px")
-     .attr("fill", "#ffffffaa")
-     .text(function(d,i) {
+    //出现百分比文字
+    axisGrid.selectAll(".axisLabel")
+    .data(d3.range(1,radarconfig.levels+1).reverse())
+    .enter().append("text")
+    .attr("class", "axisLabel")
+    .attr("x", 0)
+    .attr("y", function(d){return -d*radarconfig.maxR/radarconfig.levels;})
+    .attr("dy", "0.4em")
+    .style("font-size", "10px")
+    .attr("fill", "#ffffffaa")
+    .text(function(d,i) {
       return 100*d/radarconfig.levels+"%"; }
       );
 
+     //每个元素的父盒子
+     var axis = axisGrid.selectAll(".axis")
+     .data(allAxis)
+     .enter()
+     .append("g")
+     .attr("class", "axis");
+    //每个轴的线
+    axis.append("line")
+    .attr("x1", 0)
+    .attr("y1", 0)
+    .attr("x2", function(d, i){ return  radarconfig.maxR*Math.cos(radarconfig.angleSlice*i - Math.PI/2); })
+    .attr("y2", function(d, i){ return radarconfig.maxR* Math.sin(radarconfig.angleSlice*i - Math.PI/2); })
+    .attr("class", "line")
+    .style("stroke", "#ffffffaa")
+    .style("stroke-width", "1px");
+
+    //每个轴的名字
+    axis.append("text")
+    .attr("class", "legend")
+    .style("font-size", "10px")
+    .attr("text-anchor", "middle")
+    .attr("dy", "0em")
+    .attr('fill',"#ffffffaa")
+    .attr("x", function(d, i){ return (radarconfig.maxR+10)* Math.cos(radarconfig.angleSlice*i - Math.PI/2); })
+    .attr("y", function(d, i){ return (radarconfig.maxR+20)* Math.sin(radarconfig.angleSlice*i - Math.PI/2); })
+    .text(function(d){return d}) ;
+
+    //显示数据图
+
+    
+    
+  //Create a wrapper for the blobs  
+  var blobWrapper = g.selectAll(".radarWrapper")
+  .data(data)
+  .enter()
+  .append("g")
+  .attr("class", "radarWrapper");
+  
+  //Append the backgrounds  
+  blobWrapper
+  .append("path")
+  .attr("class", "radarArea")
+  .attr("d", function(d,i) { 
+    var path = d3.path();
+    for(let i=0;i<d.length;i++){
+      if(i==0) {
+        let x=d[i].axis/10*radarconfig.maxR*Math.cos(radarconfig.angleSlice*i - Math.PI/2);
+        let y=d[i].axis/10*(radarconfig.maxR)* Math.sin(radarconfig.angleSlice*i - Math.PI/2);
+        path.moveTo(x, y);
+      } else{
+        let x=d[i].axis*radarconfig.maxR*Math.cos(radarconfig.angleSlice*i - Math.PI/2);
+        let y=d[i].axis*(radarconfig.maxR)* Math.sin(radarconfig.angleSlice*i - Math.PI/2);
+        path.lineTo(x, y);
+      }
+    }
+    return path.toString();
+
+  })
+  .style("fill", function(d,i) { return '#ff5555'; })
+  .style("fill-opacity", 0.7);
+  
 }
   //显示中国排序
   function initChinaRank(){
@@ -194,7 +285,6 @@
        if (value) {
         d3.select(this).attr('fill', color(value));
       } else {
-
         d3.select(this).attr('fill', "#ccc");
       }
 
@@ -203,3 +293,5 @@
       .attr('stroke', 'rgba(255,255,255,1)')
       .attr('stroke-width', 1);
     }
+
+
